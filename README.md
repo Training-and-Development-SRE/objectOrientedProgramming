63716b9169f5e34bf513c015bbc81ae6203083e5
root <root@LeibowitzPC.localdomain>
2018-03-06 13:53:45 -0600

    ...

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 A series of exercises for Coding Dojo covering basic facets of OOP in Python 2.
 
 bike.py, car.py, product.py -- practice creating and instantiating classes
+store.py -- practice using two complementary classes together.
--- store.py
+++ store.py
@@ -28,3 +28,27 @@ class Product(object):
         print("Brand: " + self.brand)
         print(self.status)
         return self
+
+class Store(object):
+    def __init__(self, products, location, owner):
+        self.products = products
+        self.location = location
+        self.owner = owner
+    def add_product(self, name, price, weight, brand):
+        newProduct = Product(name, price, weight, brand)
+        self.products.append(newProduct)
+        return self
+    def remove_product(self, name):
+        for product in self.products:
+            if product.name == name:
+                self.products.remove(product)
+        return self
+    def inventory(self):
+        for product in self.products:
+            product.display()
+
+walmart = Store([], "Chicago", "Alex Leibowitz")
+walmart.add_product("shoes", 100, 5, "Nike")
+walmart.add_product("blender", 30, 5, "Blenda")
+walmart.add_product("computer", 500, 30, "IBM")
+walmart.remove_product("blender").inventory()
